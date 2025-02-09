91939a567463d08e79df5254bdfb0ff7e7c69af7
sunyuze <dashuaige991@qq.com>
2025-02-09 22:46:51 +0800

    test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -349,6 +349,20 @@ pub fn run() {
             detect_device,
             device_adb,
             device_fastboot,
+            get_phone_modem_software_version,
+            get_phone_ro_product_manufacturer,
+            get_phone_ro_product_device,
+            get_phone_ro_product_brand,
+            get_phone_hardware,
+            get_phone_kernel_qemu,
+            get_phone_product_model,
+            get_phone_system_cpu_abi,
+            get_phone_system_build_version_security,
+            get_phone_system_build_id,
+            get_phone_system_build_description,
+            get_phone_android_kernel_version,
+            get_phone_bootloader_if_start,
+            get_phone_android_sdk_version,
             get_phone_android_version])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
